89c0ea6ad13c9c3c3d1898e0c51265772369e78c
Ikrar Bagaskara <ikrarb95@gmail.com>
2023-09-30 07:26:56 +0700

    add card view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,30 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div class="flex justify-between items-center">
+                        <div class="flex items-center">
+                            <svg class="h-8 w-8 text-blue-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M21 10c0 7-4 9-9 9s-9-2-9-9"></path>
+                                <path d="M12 22s-8-2-8-11h16c0 9-8 11-8 11z"></path>
+                            </svg>
+                            <span class="ml-2 font-semibold text-lg">Debit Card</span>
+                        </div>
+                        <div class="text-xl font-semibold text-green-500">
+                            Rp. {{ number_format(Auth::user()->balance, 2) }}
+                        </div>
+                    </div>
+                    <p class="mt-4 text-gray-600 dark:text-gray-400">
+                        Account Number: 17340094587
+                    </p>
+                    <p class="mt-4 text-gray-600 dark:text-gray-400">
+                        Card Number: **** **** **** 1234
+                    </p>
+                    <p class="mt-2 text-gray-600 dark:text-gray-400">
+                        Expiry Date: 12/24
+                    </p>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
