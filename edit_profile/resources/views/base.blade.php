81e5ecea5e7f08a1a350da1db01b4b31804ed225
irfan2543 <muhammadirfansyah66@gmail.com>
2023-09-30 18:00:49 +0700

    Edit Profile Front End

---
+++ edit_profile/resources/views/base.blade.php
@@ -0,0 +1,4 @@
+@include('layouts_include_head')
+@include('layouts_include_navigation')
+@yield('content')
+@include('layouts_include_footer')
---
+++ edit_profile/resources/views/layouts/include/navigation.blade.php
@@ -0,0 +1,20 @@
+<div class="col-md-12">
+        <div class="col-md-12 dashboard">
+                <div class="navbar nav-top nav-left">
+
+                </div>
+                <div class="navbar nav-top nav-right">
+                    <ul>
+                        <li>Kelompok 3</li>
+                        <li>Home</li>
+                        <li>Logout</li>
+                    </ul>
+                </div>
+        </div>
+        <div class="col-md-12 title">
+                <div class="navbar nav-bottom">
+                    <h2>PROFILE</h2>
+                </div>
+        </div>
+    </div>
+</div>
