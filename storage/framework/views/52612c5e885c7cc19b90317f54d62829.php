14fb3ed4698fb7a32ca2815e61b63156d82ade8b
Ikrar Bagaskara <ikrarb95@gmail.com>
2023-10-02 14:39:22 +0700

    remove unwanted files

--- storage/framework/views/52612c5e885c7cc19b90317f54d62829.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH C:\Users\DesaPonggok\PhpstormProjects\K3-Simple-Laravel-HW6\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
--- storage/framework/views/73bdb20db119f65a27980fbd83b5003c.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH C:\Users\DesaPonggok\PhpstormProjects\K3-Simple-Laravel-HW6\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
